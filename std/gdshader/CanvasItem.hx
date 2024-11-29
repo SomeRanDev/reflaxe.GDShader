48bb72616af85cb6669324f2109e3663cc299320
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-11-29 11:53:09 -0500

    Mark builtin shader constants usage restrictions

--- std/gdshader/CanvasItem.hx
+++ std/gdshader/CanvasItem.hx
@@ -2,8 +2,10 @@ package gdshader;
 
 @:shaderType(canvas_item)
 extern class CanvasItem extends Shader {
-	var UV: Vec2;
-	var SCREEN_UV: Vec2;
-	var COLOR: Vec4;
-	var TEXTURE: Sampler2D;
+	@:onlyAllowIn(vertex) var INSTANCE_CUSTOM: Vec4;
+	@:onlyAllowIn(vertex, fragment, light) var UV: Vec2;
+	@:onlyAllowIn(fragment, light) var SCREEN_UV: Vec2;
+	@:onlyAllowIn(fragment) var COLOR: Vec4;
+	@:onlyAllowIn(fragment) var TEXTURE: Sampler2D;
+	@:onlyAllowIn(fragment) var SCREEN_PIXEL_SIZE: Vec2;
 }
