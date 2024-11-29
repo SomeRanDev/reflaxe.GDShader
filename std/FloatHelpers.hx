4847cb62be545615efd9188947a0e07a2712572f
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-11-28 23:06:07 -0500

    Created FloatHelpers

---
+++ std/FloatHelpers.hx
@@ -0,0 +1,9 @@
+package;
+
+import GDShader;
+
+class FloatHelpers {
+	public static extern inline function clamp(self: Float, min: Float, max: Float): Float {
+		return clamp(self, min, max);
+	}
+}
