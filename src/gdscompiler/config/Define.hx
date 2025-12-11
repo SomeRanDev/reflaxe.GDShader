34c8c397f15b984a70ddeef73aa8705918246a3c
SomeRanDev <28355157+SomeRanDev@users.noreply.github.com>
2025-12-11 00:03:50 +0000

    Build: (8b7d7ed) Purge defines copied from Reflaxe/GDScript

--- src/gdscompiler/config/Define.hx
+++ src/gdscompiler/config/Define.hx
@@ -2,44 +2,9 @@ package gdscompiler.config;
 
 enum abstract Define(String) from String to String {
 	/**
-		-D generate_godot_plugin
+		-D gdshader.template_define
 
-		If defined, a Godot plugin will be generated with the output.
+		Keeping this here to copy/replace from once I actually have real defines.
 	**/
-	var GenerateGodotPlugin = "generate_godot_plugin";
-
-	/**
-		-D godot_plugin_name=NAME
-
-		Defines the "name" entry in `plugin.cfg` if a plugin is generated.
-	**/
-	var GodotPluginName = "godot_plugin_name";
-
-	/**
-		-D godot_plugin_description=DESCRIPTION
-
-		Defines the "description" entry in `plugin.cfg` if a plugin is generated.
-	**/
-	var GodotPluginDescription = "godot_plugin_description";
-
-	/**
-		-D godot_plugin_author=AUTHOR
-
-		Defines the "author" entry in `plugin.cfg` if a plugin is generated.
-	**/
-	var GodotPluginAuthor = "godot_plugin_author";
-
-	/**
-		-D godot_plugin_version=VERSION
-
-		Defines the "version" entry in `plugin.cfg` if a plugin is generated.
-	**/
-	var GodotPluginVersion = "godot_plugin_version";
-
-	/**
-		-D godot_plugin_script_name=FILE_NAME.gd
-
-		Sets the name of the "script" file generated for the plugin.
-	**/
-	var GodotPluginScriptName = "godot_plugin_script_name";
+	var TemplateDefine = "gdshader.template_define";
 }
